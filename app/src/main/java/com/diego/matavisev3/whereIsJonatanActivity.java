d5a77d8e93971af6f1059b2f6d7e7b944edb7a51
diego <diegocalatayud.alu@iespacomolla.es>
2022-10-12 19:12:38 +0200

    Añado nueva activity

--- app/src/main/java/com/diego/matavisev3/whereIsJonatanActivity.java
+++ app/src/main/java/com/diego/matavisev3/whereIsJonatanActivity.java
@@ -1,17 +1,11 @@
 package com.diego.matavisev3;
 
-import android.annotation.SuppressLint;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.Context;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
-import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
 import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -19,18 +13,16 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class MainActivity extends AppCompatActivity
-{
+public class whereIsJonatanActivity extends AppCompatActivity {
+
     //Attributes.
     private int numberOfDeathsShown=0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_where_is_jonatan);
     }
-
     //Method that is called when the button is clicked and chooses one death to show.
     public void onClickDeath(View view)
     {
@@ -46,10 +38,6 @@ public class MainActivity extends AppCompatActivity
         String result = "Muerte Nº"+election+"\n"+death;
         TextView resultView = findViewById(R.id.textViewDeath);
         resultView.setText(result);
-
-        //Gets the number of deaths shown and increases its value.
-        numberOfDeathsShown++;
-        actionsOnDeathsShown(numberOfDeathsShown,view);
     }
 
     //Returns the line passed as a parameter.
@@ -68,6 +56,7 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
+    //TODO Adaptar todo el código a Busca a Jonatan
     public void onClickShareDeath(View view)
     {
         if(numberOfDeathsShown>0)
@@ -82,37 +71,16 @@ public class MainActivity extends AppCompatActivity
             sendIntent.setType("text/plain");
             startActivity(sendIntent);
         }
-        else{Snackbar.make(view,"Aún no se ha generado ninguna muerte.",Snackbar.LENGTH_LONG).show();}
+        else{
+            Snackbar.make(view,"Aún no se ha generado ninguna muerte.",Snackbar.LENGTH_LONG).show();}
 
     }
 
+    //TODO Arreglar esto para que vaya a la anterior.
     public void onClickChangeMode(View view)
     {
         Intent intent = new Intent(this, whereIsJonatanActivity.class);
         startActivity(intent);
     }
 
-    @SuppressLint("ResourceAsColor")
-    private void actionsOnDeathsShown(int numberOfDeathsShown, View view)
-    {
-        //Depending on how many deaths have been shown, the app does several actions.
-        switch (numberOfDeathsShown)
-        {
-            case 20:
-                Snackbar.make(view,"¡Enhorabuena! Has visto a Vise morir "+numberOfDeathsShown+" veces.",Snackbar.LENGTH_LONG).show();
-                break;
-            case 50:
-                String text = "¡Te aburres mucho! Has visto a Vise morir "+numberOfDeathsShown+" veces.";
-                Snackbar.make(view,text,Snackbar.LENGTH_LONG).show();
-                break;
-            case 75:
-                Snackbar.make(view,"¡Eres un adicto a matar a Vise!",Snackbar.LENGTH_LONG).show();
-                break;
-            case 100:
-                MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.police);
-                mediaPlayer.start();
-                Snackbar.make(view,"¡Hemos verificado que eres un psicópata, la policía va en tu dirección!",Snackbar.LENGTH_LONG).show();
-                break;
-        }
-    }
 }
